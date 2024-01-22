7c620064b06879f0cef512f86aec56259604c4b5
Milind452 <milind.mohapatra@gmail.com>
2024-01-22 20:58:04 +0530

    implement user repo

--- BuberDiner.Infrastructure/DependencyInjection.cs
+++ BuberDiner.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,8 @@
 using BuberDiner.Application.Common.Interfaces.Authentication;
+using BuberDiner.Application.Common.Interfaces.Persistence;
 using BuberDiner.Application.Common.Interfaces.Services;
 using BuberDiner.Infrastructure.Authentication;
+using BuberDiner.Infrastructure.Persistence;
 using BuberDiner.Infrastructure.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,8 +14,11 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
     {
         services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+
         services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+
+        services.AddScoped<IUserRepository, UserRepository>();
         return services;
     }
 }
