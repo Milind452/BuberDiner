c89338cab71ca32be2c8a39f8a100a2852b6b3c0
Milind452 <milind.mohapatra@gmail.com>
2024-01-24 12:35:56 +0530

    add error handling middleware

--- BuberDiner.Api/Program.cs
+++ BuberDiner.Api/Program.cs
@@ -1,3 +1,4 @@
+using BuberDiner.Api.Middleware;
 using BuberDiner.Application;
 using BuberDiner.Infrastructure;
 
@@ -11,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 {
+    app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
