bd326c1115b9a0aad94af3b38d20e4d1477f9014
Milind452 <milind.mohapatra@gmail.com>
2024-02-02 12:58:38 +0530

    use problem details object instead of custom error response

--- BuberDiner.Api/Filters/ErrorHandlingFilterAttribute.cs
+++ BuberDiner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -8,13 +9,13 @@ public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
     public override void OnException(ExceptionContext context)
     {
         var exception = context.Exception;
-        var errorResult = new
+        var problemDetails = new ProblemDetails
         {
-            error = "An error occurred while processing your request.",
-            message = exception.Message
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
+            Title = "An error occurred while processing your request.",
+            Status = (int)HttpStatusCode.InternalServerError
         };
-        context.Result = new ObjectResult(errorResult)
-        { StatusCode = 500 };
+        context.Result = new ObjectResult(problemDetails);
         context.ExceptionHandled = true;
     }
 }
